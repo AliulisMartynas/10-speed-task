4eec3984a25cba1827cf4f99c14b65c110c6ddfd
martynas <martynas.aliulis@mif.stud.vu.lt>
2022-07-24 19:37:43 +0300

    Added undo+redo functionality

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,6 +6,9 @@ import { Undo, Redo, Clear, Add } from "@mui/icons-material";
 const Form = () => {
   const [inputValue, setInputValue] = useState<string>("");
   const [inputList, setInputList] = useState<string[]>([]);
+  const [inputHistory, setInputHistory] = useState<string[]>([]);
+  const [undoActive, setUndoActive] = useState<boolean>(false);
+  const [redosRemaining, setRedosRemaining] = useState<number>(0);
 
   useEffect(() => {
     const storedInputs = localStorage.getItem("inputList");
@@ -13,6 +16,17 @@ const Form = () => {
       setInputList(JSON.parse(storedInputs));
     }
   }, []);
+
+  useEffect(() => {
+    if (
+      (inputValue && inputValue.length > 0) ||
+      (inputHistory.length > 0 && redosRemaining !== inputHistory.length)
+    ) {
+      setUndoActive(true);
+    } else {
+      setUndoActive(false);
+    }
+  }, [inputHistory, inputValue, redosRemaining]);
 
   const saveToList = () => {
     setInputList([...inputList, inputValue]);
@@ -25,19 +39,47 @@ const Form = () => {
 
   const onInputChange = (value: string) => {
     setInputValue(value);
+    setInputHistory((currentHistory) => [...currentHistory, value]);
+    setRedosRemaining(0);
+  };
+
+  const handleUndo = () => {
+    const historyIndex = inputHistory.length - 2 - redosRemaining;
+    if (historyIndex >= 0) {
+      setInputValue(inputHistory[historyIndex]);
+    } else {
+      setInputValue("");
+    }
+    setRedosRemaining(redosRemaining + 1);
+  };
+
+  const handleRedo = () => {
+    setInputValue(inputHistory[inputHistory.length - redosRemaining]);
+    setRedosRemaining(redosRemaining - 1);
   };
 
   const handleClear = () => {
     setInputValue("");
+    setInputHistory((currentHistory) => [...currentHistory, ""]);
   };
 
   return (
     <div className="App">
       <Stack direction="row" spacing={1}>
-        <Button variant="contained" startIcon={<Undo />}>
+        <Button
+          variant="contained"
+          startIcon={<Undo />}
+          onClick={handleUndo}
+          disabled={undoActive === false}
+        >
           Undo
         </Button>
-        <Button variant="contained" startIcon={<Redo />}>
+        <Button
+          variant="contained"
+          startIcon={<Redo />}
+          onClick={handleRedo}
+          disabled={redosRemaining < 1}
+        >
           Redo
         </Button>
         <Button
